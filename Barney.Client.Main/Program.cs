bb7c81add0d44c911775329e5a1084b0efb3f574
Justin Halse <justin@solutions365.co.za>
2019-09-17 01:54:40 +0200

    Add project files.

---
+++ Barney.Client.Main/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Barney.Client.Main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
